0e298c022841e6066519eb68a9150d5991df6f44
ultram4rine <ultramarine730@gmail.com>
2021-12-17 15:11:30 +0400

    concatenation-of-array in rust

---
+++ easy/concatenation-of-array/rust/main.rs
@@ -0,0 +1,15 @@
+fn main() -> () {}
+
+impl Solution {
+    pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len();
+        let mut ans = vec![0; 2 * n];
+
+        for (i, num) in nums.clone().iter().enumerate() {
+            ans[i] = *num;
+            ans[i + n] = *num;
+        }
+
+        ans
+    }
+}
